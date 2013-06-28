fcefc832d9d8c990006e18cd3a2f081e673af916
Kevin <kevinhughes27@gmail.com>
2013-06-28 15:13:39 -0400

    added the training framework and a skeleton for the road segmentation alg, also gitignore for pyc files

---
+++ segmentRoad.py
@@ -0,0 +1,5 @@
+import cv2
+
+def segmentRoad(frame):
+    cv2.imshow("frame", frame)
+    cv2.waitKey(100)
--- trainAI.py
+++ trainAI.py
@@ -1,1 +1,45 @@
 #!/usr/bin/env python
+
+import sys
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+from segmentRoad import segmentRoad
+
+def main():
+
+    if len(sys.argv) != 2:
+        print 'Usage: python trainAI.py <data_folder>'
+
+    dataDir = sys.argv[1]
+    
+    if dataDir[-1] != '/':
+        dataDir+='/'
+    
+    info = open(dataDir+'info.txt')
+    info.readline()
+    start = int(info.readline())
+    end = int(info.readline())
+    
+    vid = cv2.VideoCapture()
+    vid.open(dataDir+'img_%0d.png')
+    vid.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, start)
+    
+    joystickVals = np.loadtxt(dataDir+'joystick.csv', delimiter=',')[start:-1,1:]
+    
+    i = start
+    while(i <= end):
+        
+        flag, frame = vid.read()
+        
+        segmentRoad(frame)
+        
+        #joystickVals[i-start,:]
+        
+        i += 1
+        
+    return       
+        
+if __name__ == '__main__':
+    main()
